9215aefb51049b7e7f93a9e78442504cc8c50e5e
Jonathan Coleman <jcoleman3307@gmail.com>
2015-10-08 23:17:27 +0000

    through Server Side Controller

---
+++ app/controllers/clickController.client.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+   var addButton = document.querySelector('.btn-add');
+   var deleteButton = document.querySelector('.btn-delete');
+   var clickNbr = document.querySelector('#click-nbr');
+   var apiUrl = 'http://localhost:3000/api/clicks';
+})();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
-module.exports = function (app) {
+var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+
+module.exports = function (app, db) {
+
+    var clickHandler = new ClickHandler(db);
+
     app.route('/')
         .get(function (req, res) {
             res.sendFile(process.cwd() + '/public/index.html');
         });
+
+    app.route('/api/clicks')
+        .get(clickHandler.getClicks)
+        .post(clickHandler.addClick)
+        .delete(clickHandler.resetClicks);
 };
