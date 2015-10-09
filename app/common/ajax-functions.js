84a60b5925f5b1cbf3f27954a7da7cd63d6077f0
Jonathan Coleman <jcoleman3307@gmail.com>
2015-10-09 18:56:08 +0000

    finished

---
+++ app/common/ajax-functions.js
@@ -0,0 +1,26 @@
+appUrl = 'https://basejump1-jondcoleman.c9.io/';
+ajaxFunctions = {
+   ready: function ready (fn) {
+      if (typeof fn !== 'function') {
+         return;
+      }
+
+      if (document.readyState === 'complete') {
+         return fn();
+      }
+
+      document.addEventListener('DOMContentLoaded', fn, false);
+   },
+   ajaxRequest: function ajaxRequest (method, url, callback) {
+      var xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            callback(xmlhttp.response);
+         }
+      };
+
+      xmlhttp.open(method, url, true);
+      xmlhttp.send();
+   }
+};
--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -1,57 +1,35 @@
 'use strict';
 
 (function () {
+   
+   //var ajaxFunctions = require('../common/ajax-functions.js');
+   
    var addButton = document.querySelector('.btn-add');
    var deleteButton = document.querySelector('.btn-delete');
    var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = 'https://basejump1-jondcoleman.c9.io/api/clicks';
-   
-   function ready (fn) {
-      if (typeof fn !== 'function') {
-         return;
-      }
-   
-      if (document.readyState === 'complete') {
-         return fn();
-      }
-   
-      document.addEventListener('DOMContentLoaded', fn, false);
-   }
-   
-   function ajaxRequest (method, url, callback) {
-      var xmlhttp = new XMLHttpRequest();
+   var apiUrl = appUrl + 'api/:id/clicks';
 
-      xmlhttp.onreadystatechange = function () {
-         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            callback(xmlhttp.response);
-         }
-      };
-
-      xmlhttp.open(method, url, true);
-      xmlhttp.send();
-   }
-   
    function updateClickCount (data) {
       var clicksObject = JSON.parse(data);
       clickNbr.innerHTML = clicksObject.clicks;
    }
-   
-   ready(ajaxRequest('GET', apiUrl, updateClickCount));
-   
+
+   ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount));
+
    addButton.addEventListener('click', function () {
 
-      ajaxRequest('POST', apiUrl, function () {
-         ajaxRequest('GET', apiUrl, updateClickCount)
+      ajaxFunctions.ajaxRequest('POST', apiUrl, function () {
+         ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount);
       });
 
    }, false);
-   
+
    deleteButton.addEventListener('click', function () {
 
-      ajaxRequest('DELETE', apiUrl, function () {
-         ajaxRequest('GET', apiUrl, updateClickCount);
+      ajaxFunctions.ajaxRequest('DELETE', apiUrl, function () {
+         ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount);
       });
 
    }, false);
-   
+
 })();
