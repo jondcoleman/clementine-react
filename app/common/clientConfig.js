db7b15dea1ed5c062a44e157086930b93e939105
Jonathan Coleman <jcoleman3307@gmail.com>
2015-10-11 23:01:10 +0000

    switch to heroku

--- app/common/clientConfig.js
+++ app/common/clientConfig.js
@@ -1,4 +1,4 @@
 'use strict'
 
-var appUrl = 'https://basejump1-jondcoleman.c9.io/';
-//var appUrl = 'https://whispering-ocean-6392.herokuapp.com/';
+//var appUrl = 'https://basejump1-jondcoleman.c9.io/';
+var appUrl = 'https://whispering-ocean-6392.herokuapp.com/';
